60f4199fc98e333c9655adaaf4acd3e8a9ed10a3
sokoliurii <sokoliurii@gmail.com>
2019-10-28 15:15:30 +0300

    Update config.js

--- source/REST/config.js
+++ source/REST/config.js
@@ -1,5 +1,5 @@
 // Config
 export const ROOT_URL = 'https://lab.lectrum.io';
 export const MAIN_URL = `${ROOT_URL}/redux/api`;
-export const groupId = 'medlgvgc4fn5';
-export const invite = '4627732';
+export const groupId = '4luucyyhwlwd';
+export const invite = 'aVfFDYNsqyXy';
